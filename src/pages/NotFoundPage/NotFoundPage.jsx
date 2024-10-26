5047e071f8c52a39d3f9c2a6a1600bbb51b0faef
Денис Уцеха <d.utsekha@reactor.com.ua>
2024-10-26 16:56:49 +0300

    feat: add 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 - Page not found</h1>
-      <Link to="/">Home</Link>
-    </div>
+    <>
+      <Header />
+      <div className="text-center pt-20">
+        <h1 className="text-h1 mb-16">404 - Page not found</h1>
+        <Link to="/" className="underline">
+          Go home
+        </Link>
+      </div>
+    </>
   );
 }
